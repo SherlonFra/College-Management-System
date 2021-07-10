cd729112cd08193e82fc82c6743ec4d291ce5e99
SherlonFra <80291777+SherlonFra@users.noreply.github.com>
2021-07-10 19:37:35 -0400

    Initial commit

---
+++ College Management System/College Management System/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using College_Management_System.Models;
+
+namespace College_Management_System.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Student> Students  { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ College Management System/College Management System/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace College_Management_System.Models
+{
+    public class Student
+    {
+        [Required]
+        public int StudentId { get; set; }
+        [Required]
+        public string FirstName { get; set; } 
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public DateTime StudentBirthdate { get; set; }
+        
+        [Required]
+        public int PhoneNumber { get; set; }
+        
+        [Required]
+        public string Country { get; set; }
+
+
+
+    }
+}
